88de8e6ce1905520f3162363adeb40ae7ca72a27
jefferson klamas <jeffersonklamas@gmail.com>
2020-11-09 22:46:38 -0300

    Definindo os movimentos da cobrinha

--- Estudo_de_javascript/recriando_jogo_cobrinha_Javascript/script.js
+++ Estudo_de_javascript/recriando_jogo_cobrinha_Javascript/script.js
@@ -6,6 +6,7 @@ snake [0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right"; /*Definindo a direção */
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -19,6 +20,27 @@ function criarCobrinha(){
     }
 }
 
+function iniciarjogo(){
 criarBG();
 criarCobrinha();
 
+let snakeX = snake[0].x;
+let snakeY = snake[0].y;
+
+if(direction == "right") snakeX += box;
+if(direction == "left") snakeX -= box;
+if(direction == "up") snakeY -= box;
+if(direction == "down") snakeY += box;
+
+snake.pop();
+
+let newHead = {
+    x: snakeX,
+    y: snakeY
+}
+
+snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarjogo, 100); /*Intervalo de 100 milesegundos para se renovar
+ */
