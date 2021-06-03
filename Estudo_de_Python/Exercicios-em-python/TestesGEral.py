b5360c5127a6de5b8ccc363e1ab8bff2fe398c87
jeffersonklamas <jeffersonklamas@gmail.com>
2021-06-03 11:13:39 -0300

    Testes em python.

--- Estudo_de_Python/Exercicios-em-python/TestesGEral.py
+++ Estudo_de_Python/Exercicios-em-python/TestesGEral.py
@@ -1,7 +1,18 @@
-a = int(input("Qual o valor de a? "))
+""" a = int(input("Qual o valor de a? "))
 b = int(input("Qual o valor de b? "))
 aux = a
 a = b
 b = aux
 print("O valor de a é",a)
-print("O valor de b1 é",b)
+print("O valor de b1 é",b) """
+
+""" s = 0
+for d in range(0, 5, 0.1):
+    s += d
+    print(s) """
+    
+a = 60
+b = 30
+c = b if (b > a) else a
+print(b)
+print(c)
